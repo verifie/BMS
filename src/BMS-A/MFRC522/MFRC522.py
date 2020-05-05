fa3fa0f71c78dd3700c5d27e5c890c1e9823c3d7
Paul Emerton <paulemerton@gmail.com>
2020-05-05 21:14:33 +0100

    Print all received data at this stage.

--- src/BMS-A/MFRC522/MFRC522.py
+++ src/BMS-A/MFRC522/MFRC522.py
@@ -357,17 +357,20 @@ class MFRC522:
     recvData.append(self.PICC_READ)
     recvData.append(blockAddr)
 
-    print ("  Received data :           [", recvData, "]")
-    print ("  - Read Sector Number:     [", blockAddr, "]")
+    print ("  Received data :           ", recvData,)
+    print ("  - Read Sector Number:     ", blockAddr)
 
     pOut = self.CalulateCRC(recvData)
-    print ("  - Read pOut:              [", pOut, "]")
+    print ("  - Read pOut:              ", pOut)
     recvData.append(pOut[0])
     recvData.append(pOut[1])
 
     (status, backData, backLen) = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, recvData)
     
-    print ("  - Read Sector Data:       [", backData, "]")
+
+    print ("  - Read status:            ", status)
+    print ("  - Read backData:          ", backData)
+    print ("  - Read backLen:           ", backLen)
 
     if not(status == self.MI_OK):
       print ("Error while reading!")
