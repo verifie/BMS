f6995c783df87342e5326a8246037103d142f29b
Paul Emerton <paulemerton@gmail.com>
2020-05-10 22:39:09 +0100

    Amend MFC code to review the sector / element reading loop.

--- src/BMS-A/MFRC522/MFRC522.py
+++ src/BMS-A/MFRC522/MFRC522.py
@@ -384,7 +384,7 @@ class MFRC522:
           
         print ("  - Data elements in array (+1): [", rangePeak, "]")
 
-        for i in (0, rangePeak):
+        for i in (0, rangePeak, 1):
           print ("  - Read backData [", i, "]:      ", backData[i])
 
       print ("  - Read backLen:           ", backLen)
