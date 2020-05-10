ae1c8158b064b04c9394e19f7cdea1eb6e6e06ee
Paul Emerton <paulemerton@gmail.com>
2020-05-10 20:50:11 +0100

    As anticipated, 7 failed. Pushing back upto 8 as we know works, but go beyond to 32 to test the point at which the reads cease working. Its worth noting a failed read cascades across all other reads, even when that read would normally work in isolation.

--- src/BMS-A/MFRC522/PRead2.py
+++ src/BMS-A/MFRC522/PRead2.py
@@ -119,7 +119,7 @@ while continue_reading:
         # Check if authenticated
         if status == MIFAREReader.MI_OK:
 
-            for i in range(7, 11):
+            for i in range(8, 32):
                 print ("authenticated OK, now read sector [ ", i, " ]")
                 MIFAREReader.MFRC522_Read(i)
                 print (" ")
