af53c5ce9658f6cbeef5a0b6f7ad7c8357058989
Paul Emerton <paulemerton@gmail.com>
2020-05-10 20:57:07 +0100

    So we have discovered 8-11 read OK. before or after causes a failure.  This could be because there aren't other sectors that are open for read or write, or it could be because other sectors require different authentication.  But testing 8-11 in this version to confirm no failed reads.

--- src/BMS-A/MFRC522/PRead2.py
+++ src/BMS-A/MFRC522/PRead2.py
@@ -119,7 +119,7 @@ while continue_reading:
         # Check if authenticated
         if status == MIFAREReader.MI_OK:
 
-            for i in range(8, 32):
+            for i in range(8, 12):
                 print ("authenticated OK, now read sector [ ", i, " ]")
                 MIFAREReader.MFRC522_Read(i)
                 print (" ")
