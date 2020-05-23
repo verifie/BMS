7bd04748dc39fe4e80e693c9abe496b13b4eb6ad
Paul Emerton <paulemerton@gmail.com>
2020-05-23 22:18:25 +0100

    Lighting control interface trials - MCP23017 I2C Port Expander

--- src/BMS-A/MFRC522/BMS-L.py
+++ src/BMS-A/MFRC522/BMS-L.py
@@ -42,7 +42,7 @@ import smbus
 #bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
 bus = smbus.SMBus(1) # Rev 2 Pi uses 1
  
-DEVICE = 0x20 # Device address (A0-A2)
+DEVICE = 0x22 # Device address (A0-A2)
 IODIRA = 0x00 # Pin direction register
 OLATA  = 0x14 # Register for outputs
 GPIOA  = 0x12 # Register for inputs
