8fbae2a4f70f74233357182b78b23f5c34b760f1
Paul Emerton <paulemerton@gmail.com>
2020-05-27 13:09:16 +0100

    Exploring switch state change logic detection and actions. Bugfix.

--- src/BMS-A/MFRC522/BMS-L.py
+++ src/BMS-A/MFRC522/BMS-L.py
@@ -96,7 +96,7 @@ class bmsl(object):
 
 
     changeCircuitState = False
-    MySwitchPrevious = 0
+    MySwitchCurrentState = 0
 
     room_light_circuit_A = 0x00
     room_light_circuit_A_status = False
@@ -247,14 +247,14 @@ class bmsl(object):
     #########################################################################################################################################   
     # actionTrigger
     #
-    # This is called if a trigger has been found
+    # This is called if a trigger has been found. It checks to see if it was already requested and actions if not.
 
-    def actionTrigger(self, request, previousState):
+    def actionTrigger(self):
         
         # A trigger passed our tests and appeared genuine.
 
-        # Compare this trigger to the previous trigger.  If it is different, do something, else ignore.        
-        if not request == previousState:
+        # Compare this trigger to the previous trigger.  If it is NOT the same, do what is asked and record that as the current state, else ignore.        
+        if not self.MySwitch == self.MySwitchCurrentState:
         
             # The trigger is different, so we will show the trigger.
             print ("A new trigger was acknowledged.  Bus Read Status : ", self.MySwitch)
@@ -308,7 +308,7 @@ class bmsl(object):
             if self.changeCircuitState:
 
                 # A trigger request was made. #TODO Pass request on to action.
-                self.actionTrigger(self.MySwitch, self.MySwitchCurrentState)
+                self.actionTrigger()
 
             # End of RunProgram Loop. Restarting.
 
