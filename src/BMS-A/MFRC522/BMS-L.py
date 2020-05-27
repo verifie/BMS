2ff0aaf9292f1b7545e9acdb14685ccb7475ccc7
Paul Emerton <paulemerton@gmail.com>
2020-05-27 12:50:16 +0100

    Exploring switch state change logic detection and actions. Bugfix.

--- src/BMS-A/MFRC522/BMS-L.py
+++ src/BMS-A/MFRC522/BMS-L.py
@@ -249,12 +249,12 @@ class bmsl(object):
     #
     # This is called if a trigger has been found
 
-    def actionTrigger(self):
+    def actionTrigger(self, request):
         
         # A trigger passed our tests and appeared genuine.
 
         # Compare this trigger to the previous trigger.  If it is different, do something, else ignore.        
-        if not self.Myswitch == self.MySwitchCurrentState:
+        if not request == self.MySwitchCurrentState:
         
             # The trigger is different, so we will show the trigger.
             print ("A new trigger was acknowledged.  Bus Read Status : ", self.MySwitch)
@@ -308,7 +308,7 @@ class bmsl(object):
             if self.changeCircuitState:
 
                 # A trigger request was made. #TODO Pass request on to action.
-                self.actionTrigger()
+                self.actionTrigger(self.MySwitch)
 
             # End of RunProgram Loop. Restarting.
 
