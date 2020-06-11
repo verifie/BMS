36d67d9cdcd5b2d433eb56ca5faf0426f893e53c
Paul Emerton <paulemerton@gmail.com>
2020-06-11 22:09:33 +0100

    Developing binary conversion to decimal then hex, ready to write back to the remote GPIO chip.

--- src/BMS-L/src/Formula.py
+++ src/BMS-L/src/Formula.py
@@ -65,6 +65,36 @@ class Formula(object):
 
 
 
+
+
+    #########################################################################################################################################    
+    # When addressing the external GPIO interfaces, we address a block of 8 each time in binary.  The first digit is Pin 0, last is pin 7, etc.
+    # However, by default, the information is presented as a decimal, which is informative but not helpful for a quick human determination of
+    # what is going on.  Ideally, we will also deal with it as a sequence of binary digits as opposed to a decimal to make the program easier
+    # to read.
+    def binary(self, num, pre='0b', length=8, spacer=0):
+
+        binaryConverted = str('{0}{{:{1}>{2}}}'.format(pre, spacer, length).format(bin(num)[2:]))
+        
+        # DEBUG - Verbose announcer.
+        if v.debug_verbose:
+            print("[DEBUG]   binaryConverted :", binaryConverted)
+                    
+            bitCount = 0
+            for i in range(9, 1, -1):
+                print ("[LOG]     Bit ", bitCount," Bus Read Status : ", binaryConverted[i])
+                bitCount = bitCount + 1
+
+        # DEBUG end
+
+        # Now count the binary string and convert into Hex.
+        binaryStringToHex(binaryConverted)
+
+        return binaryConverted
+
+
+
+
     #########################################################################################################################################    
     # DiscoverHex
     # We'll do this manually, even though it is almost certain that an inbuilt function does this already!
@@ -96,35 +126,7 @@ class Formula(object):
 
         return hexToWrite
 
-
-
-    #########################################################################################################################################    
-    # When addressing the external GPIO interfaces, we address a block of 8 each time in binary.  The first digit is Pin 0, last is pin 7, etc.
-    # However, by default, the information is presented as a decimal, which is informative but not helpful for a quick human determination of
-    # what is going on.  Ideally, we will also deal with it as a sequence of binary digits as opposed to a decimal to make the program easier
-    # to read.
-    def binary(self, num, pre='0b', length=8, spacer=0):
-
-        binaryConverted = str('{0}{{:{1}>{2}}}'.format(pre, spacer, length).format(bin(num)[2:]))
         
-        # DEBUG - Verbose announcer.
-        if v.debug_verbose:
-            print("[DEBUG]   binaryConverted :", binaryConverted)
-                    
-            bitCount = 0
-            for i in range(9, 1, -1):
-                print ("[LOG]     Bit ", bitCount," Bus Read Status : ", binaryConverted[i])
-                bitCount = bitCount + 1
-
-        # DEBUG end
-
-        # Now count the binary string and convert into Hex.
-        binaryStringToHex(binaryConverted)
-
-        return binaryConverted
-
-
-
        
     #########################################################################################################################################   
     # convertBinaryString
