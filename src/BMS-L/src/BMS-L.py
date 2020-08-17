da1679df0728d76614529406751f03a24182f7c7
Paul Emerton <paulemerton@gmail.com>
2020-08-17 23:36:05 +0100

    # 2020/08/17 2136 v0.09 PME - Improve debug logs with library and function information.

--- src/BMS-L/src/BMS-L.py
+++ src/BMS-L/src/BMS-L.py
@@ -120,7 +120,6 @@ class bmsl(object):
         if v.debug_verbose:
             print ("\n\n[ACTION]  A lighting state change was acknowledged.")
 
-        print ("\n\n -- ")
         print ("[LOG]     Current date and time : ", now.strftime("%Y-%m-%d %H:%M:%S"))
 
         # Increment the action tally
